6ddad60519d672e56ad6b60d8a0ee9e231fc53b1
Your Username <you@example.com>
2021-03-10 16:29:43 -0500

    fetch requests pulls up recipe attributes

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
+const recipeContainer = document.querySelector("#recipe-container")
+
 document.addEventListener('DOMContentLoaded', () => {
-    getRecipeData()
+    getRecipe()
+  
 })
 
-function getRecipeData() {
+function getRecipe() {
     // debugger;
     fetch ("http://localhost:3000/api/v1/recipes")
         .then(resp => resp.json())
-        .then(data => console.log(data))
+        .then(recipe => {
+            // debugger;
+            recipe.data.forEach(recipeData => {
+                let recipeHTML = `
+                <div data-id=${recipe.data[0].id}>
+                <h1>${recipe.data[0].attributes.title}</h1>
+                <h3>${recipe.data[0].attributes.description}</h3>
+                <img src=${recipe.data[0].attributes.image_url}></img>
+                </div>   
+                `
+                recipeContainer.innerHTML = recipeHTML
+            })
         .catch(function(error) {
             alert("Error. Failed to fetch");
             console.log(error.message);
+        })
     })
 }
+
+
+
+
+
