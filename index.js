8347118863fc75abcb4940ac3457a7f797f35757
Your Username <you@example.com>
2021-03-10 13:52:47 -0500

    created fetch request

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("in index.js")
+document.addEventListener('DOMContentLoaded', () => {
+    getRecipeData()
+})
+
+function getRecipeData() {
+    // debugger;
+    fetch ("http://localhost:3000/api/v1/recipes")
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+        .catch(function(error) {
+            alert("Error. Failed to fetch");
+            console.log(error.message);
+    })
+}
