f2be21e533fd9628183fba0c9647b514f0f8963d
Your Username <you@example.com>
2021-03-15 18:21:11 -0400

    added renderRecipes() to Recipe class

--- recipe.js
+++ recipe.js
@@ -5,6 +5,16 @@ class Recipe {
         this.image_url = recipeAttributes.image_url;
         this.cuisine = recipeAttributes.cuisine;
     }
+
+    renderRecipes() {
+        return `
+        <div data-id=${this.id}>
+        <h1 id="recipe-title">${this.title}</h1>
+        <h3 id="recipe-description">${this.description}</h3>
+        <img id="recipe-image" src=${this.image_url} height="200" width="250"></img>
+        </div>   
+        `
+    }
 }
 
 
