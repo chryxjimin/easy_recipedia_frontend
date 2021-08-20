15c797271880259304fb99ae8f15d2d9aa809509
Your Username <cleex1999@gmail.com>
2021-08-20 12:20:15 -0400

    changed the font size

--- recipe.js
+++ recipe.js
@@ -11,8 +11,8 @@ class Recipe {
         return `
         <div class="card" style="width: 27rem;" data-id=${this.id}>
             <div class="card-body">
-                <h1 class="card-title" id="recipe-title">${this.title}</h1>
-                <h3 class="card-text" id="recipe-description">${this.description}</h3><br>
+                <h1 class="card-title" id="recipe-title"><p class="fs-3">${this.title}</p></h1>
+                <h3 class="card-text" id="recipe-description"><p class="fs-4">${this.description}</p></h3><br>
                 <img class="card-img-top" id="recipe-image" src=${this.image_url} height="200" width="250"></img><br></br>
                 <button data-id=${this.id} type="button" class="delete-button">Delete</button>
             </div>
